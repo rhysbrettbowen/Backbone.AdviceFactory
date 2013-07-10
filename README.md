162c3dcd0e86ffb9af78285288f4a7c013d84b73
Rhys <rhys@dataminr.com>
2013-07-10 16:01:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Backbone.AdviceFactory #
+
+For use with [Backbone.Advice](https://github.com/rhysbrettbowen/Backbone.Advice).
+
+When mixing inheritance models (Backbone's extends and Backbone.Advice's mixins) there are often cases when you may overwrite mixins unintentionally. Backbone.AdviceFactory helps set up the inheritance the way you want it to work.
+
+For instance you may extend "initialize" in a latter class that will not only override the former "initialize" method but also all the other mixins that were put on that function. To prevent this happenning Backbone.AdviceFactory allows you to setup an inheritance structure that will compose all the extends THEN all the mixins together for that level rather than trying to just extend a constructor that already has mixins put on.
+
+```javascript
+
+define(['Backbone.AdviceFactory'], function(Factory) {
+
+	// register a base:
+	Factory.register('view', {
+		base: Backbone.View
+	});
+
+	// you can extend and mixin
+	// it will pass back the new constructor
+	var myView = Factory.register('myView', {
+		base: 'view',
+
+		// non reserved keywords are mixed in if functions
+		defaultSize: 10,
+
+		// or you can pass in the extends
+		// such as constructors (as they're functions you don't want mixed in)
+		extend: {
+			itemView: itemView
+		},
+
+		// functional mixins go here
+		mixins: [
+			myMixin1,
+			myMixin2
+		],
+
+		// also any other advice keywords such as after, before & clobber
+		addToObj: {
+			events: {
+				'click': 'onClick'
+			}
+		}
+	});
+
+	Factory.register('myView2', {
+		base: 'myView',
+
+		// this will mixin as "after" automatically
+		initialize: function() {}
+	});
+
+	// to get the finished product:
+	var myView2inst = new Factory.get('myView2')(arg1);
+
+	// or better yet
+	var myView2inst2 = Factory.inst('myView2', arg1);
+
+});
+
+```
+
+This is still experimental the api may change (or more likely expand to be more flexible) and there are more features I'm working on such as converting functions so they take their "super" as the first argument.
+
---
+++ advicefactory.js
@@ -0,0 +1,224 @@
+// v0.1.0
+
+// ==========================================
+// Copyright 2013 Dataminr
+// Licensed under The MIT License
+// http://opensource.org/licenses/MIT
+// ==========================================
+
+define([
+	'backbone',
+	'underscore',
+	'Backbone.Advice'
+], function(Backbone, _) {
+
+	var cache = {};
+
+	var adviceKeywords = [
+		'after',
+		'before',
+		'around',
+		'clobber',
+		'addToObj',
+		'setDefaults',
+		'mixin'
+	];
+
+	var reserved = adviceKeywords.concat([
+		'mixins',
+		'extend',
+		'base',
+		'super',
+		'options'
+	]);
+
+	/**
+	 * @constructor
+	 * @param {string|function(new:Object)} base
+	 * @param {Object=} ext
+	 * @param {Object=} mixins
+	 * @param {Object=} options
+	 */
+	var BaseNode = function BaseNode(base, ext, mixins, options) {
+		this.base = _.isString(base) ? cache[base] : base;
+		this.ext = ext || {};
+		this.mixins = mixins || {};
+		this.options = options || {};
+		this.main = null;
+	};
+
+	return {
+		/**
+		 * register a type with the factory
+		 * @param {string} name
+		 * @param {Object} options
+		 * @return {function(new:Object)}
+		 */
+		register: function(name, options) {
+			var opts = {};
+
+			opts.base = _.isString(options.base) ?
+				cache[options.base] :
+				new BaseNode(options.base);
+
+			_.extend(opts, {
+				extend: _.clone(options.extend) || {},
+				mixins: _.clone(_.isArray(options.mixins) ?
+					{mixin: options.mixins,
+						after: {},
+						before: {}
+					} :
+					options.mixins) || {},
+				options: _.clone(options.options) || {}
+			});
+			for (var key in options) {
+				if (!_.contains(reserved, key)) {
+					if (_.isFunction(this.getExt(opts.base)[key]) ||
+						_.isFunction(this.getBase(opts.base)[key]))
+						opts.mixins.after[key] = options[key];
+					else
+						opts.extend[key] = options[key];
+				}
+				if (_.contains(adviceKeywords, key)) {
+					opts.mixins[key] = opts.mixins[key] || {};
+					_.extend(opts.mixins[key], options[key]);
+				}
+			}
+			cache[name] = new BaseNode(opts.base, opts.extend, opts.mixins, opts.options);
+			cache[name].main = this.create(name);
+			return cache[name].main;
+		},
+		/**
+		 * @param {string} name
+		 * @return {function(new:Object)}
+		 */
+		get: function(type) {
+			return cache[type].main;
+		},
+		/**
+		 * @param {string} name
+		 * @oaram {...*} var_args
+		 * @return {Object}
+		 */
+		inst: function(type) {
+			if (arguments.length == 1)
+				return new (this.get(type))();
+			if (arguments.length == 2)
+				return new (this.get(type))(arguments[1]);
+			if (arguments.length == 3)
+				return new (this.get(type))(arguments[1], arguments[2]);
+			if (arguments.length == 4)
+				return new (this.get(type))(arguments[1], arguments[2], arguments[3]);
+			if (arguments.length == 5)
+				return new (this.get(type))(arguments[1], arguments[2], arguments[3], arguments[4]);
+		},
+		/**
+		 * EXPERIMENTAL
+		 * mark a function to use super
+		 * @param {Function} fn
+		 */
+		super: function(fn) {
+			fn.super = true;
+		},
+		/**
+		 * EXPERIMENTAL
+		 * return a function that gets passed it's super function
+		 * @param {string} BaseNode [description]
+		 * @param {string} name of function
+		 * @param {Function} fn
+		 * @return {Function}
+		 */
+		getSuper: function(BaseNode, name, fn) {
+			var newBase = BaseNode.base;
+			var earlier = getExt(newBase)[name];
+			while (earlier == fn) {
+				newBase = newBase.base;
+				earlier = getExt(newBase)[name];
+			}
+			earlier = earlier || _.identity;
+			if (earlier.super)
+				earlier = this.getSuper(newBase, name, earlier);
+			return function() {
+				return fn.apply(this, [earlier].concat([].slice.call(arguments, 0)));
+			};
+		},
+		/**
+		 * extends mixin objects
+		 * @param {Object} obj
+		 * @param {Object} mixin
+		 * @return {Object}
+		 */
+		extendMixinObj: function(obj, mixin) {
+			for (var i = 0; i < adviceKeywords.length; i++) {
+				var key = adviceKeywords[i];
+				if (mixin[key]) {
+					if (key != 'mixin') {
+						obj[key] = obj[key] || {};
+						_.extend(obj[key], mixin[key]);
+					} else {
+						obj[key] = _.union(obj[key] || [], mixin[key]);
+					}
+				}
+			}
+			return obj;
+		},
+		/**
+		 * get mixin object for a BaseNode
+		 * @param {BaseNode} node
+		 * @return {Object}
+		 */
+		getMixins: function(node) {
+			if (!(node instanceof BaseNode)) {
+				return {}
+			}
+			return this.extendMixinObj(this.getMixins(node.base), node.mixins);
+		},
+		/**
+		 * get the options for mixins for a BaseNode
+		 * @param {BaseNode} node
+		 * @return {Object}
+		 */
+		getOptions: function(node) {
+			if (!(node instanceof BaseNode)) {
+				return {}
+			}
+			return _.extend({}, this.getOptions(node.base), node.options);
+		},
+		/**
+		 * get the extends for a BaseNode
+		 * @param {BaseNode} node
+		 * @return {Object}
+		 */
+		getExt: function(node) {
+			if (!(node instanceof BaseNode)) {
+				return {}
+			}
+			return _.extend({}, this.getExt(node.base), node.ext);
+		},
+		/**
+		 * get the base constructor for a BaseNode
+		 * @param {BaseNode} node
+		 * @return {function(new:Object)}
+		 */
+		getBase: function(node) {
+			if (!(node.base instanceof BaseNode)) {
+				return node.base;
+			}
+			return this.getBase(node.base);
+		},
+		/**
+		 * create a constructor using a BaseNode
+		 * @param {string} type
+		 * @param {Object=} options
+		 * @return {function(new:Object)}
+		 */
+		create: function(type, options) {
+			return this.getBase(cache[type])
+				.extend(this.getExt(cache[type]))
+				.mixin(this.getMixins(cache[type]),
+					_.extend({}, this.getOptions(cache[type]), options));
+		}
+
+	};
+
+});
